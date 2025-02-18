dca26967faaa4785e661e2cd86d3d7aa862a1769
Molly Von Behren <vbamalie@gmail.com>
2025-02-17 21:01:25 -0500

    initial backend

---
+++ backend/src/test/java/com/shifts/backend/ShiftAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shifts.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
