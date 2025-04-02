eb43f5147e6045ffb2d13486ff10686508fad954
Molly Von Behren <vbamalie@gmail.com>
2025-04-02 17:28:10 -0400

    started edit weekly schedule

--- frontend/src/pages/EditWeeklySchedule.tsx
+++ frontend/src/pages/EditWeeklySchedule.tsx
@@ -1,11 +1,17 @@
 import { useParams } from "react-router-dom";
+import WeekSchedule from "./components/WeekSchedule";
+import { Box } from "@mui/material";
+import EmployeeList from "./components/EmployeeList";
 
 export default function EditWeeklySchedule() {
     const params = useParams();
-    
+
   return (
-    <>
-    
-    </>
+    <Box id='edit-schedule'>
+    <WeekSchedule date={params.date}/>
+    <Box id="employee-list">
+    <EmployeeList/>
+    </Box>
+    </Box>
   )
 }
